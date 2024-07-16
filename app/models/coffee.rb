fe21672b97e411a19aa2fedd63d456dd35599ab1
Frederico Qasem Rodrigues De Sousa <fredesousa@icloud.com>
2024-07-16 17:38:43 +0200

    photo cloudinary

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -15,7 +15,6 @@ class Coffee < ApplicationRecord
   validates :roaster, presence: true
   validates :strength, presence: true
   validates :coffee_type, presence: true
-  validates :machine_type, presence: true
   validates :machin_type, presence: true
   validates :strength, inclusion: { in: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] }
   validates :machin_type, inclusion: { in: MACHINS_TYPE }
