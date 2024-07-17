c10d508651d02f80092b2fdf21e6bd34102e4a17
Frederico Qasem Rodrigues De Sousa <fredesousa@icloud.com>
2024-07-17 13:01:30 +0200

    modif routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: 'pages#home'
-  resources :users do
-    resources :preferences, only: [:new, :create] do
-      resources :coffees, only: [:index, :show]
-    end
-  end
   resources :coffees, only: [:index, :show, :new, :create, :edit, :update]
 end
