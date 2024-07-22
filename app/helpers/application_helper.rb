8b10be5dc4833ffd333fb21d6a440f7740e98fdb
jonathan delvig <delvigjon@hotmail.fr>
2024-07-22 19:20:32 +0200

    updateshow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def star_rating(rating)
+    full_star = '<i class="fa-solid fa-star" style="color: #ffdd57;"></i>'
+    empty_star = '<i class="fa-solid fa-star" style="color: #d3d3d3;"></i>'
+    
+    (full_star * rating + empty_star * (5 - rating)).html_safe
+  end
 end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,59 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const stars = document.querySelectorAll(".star");
+  stars.forEach(star => {
+    star.addEventListener("click", setRating);
+    star.addEventListener("mouseover", addHoverClass);
+    star.addEventListener("mouseout", removeHoverClass);
+  });
+
+  function setRating(event) {
+    const clickedStar = event.target;
+    const stars = Array.from(clickedStar.parentNode.children);
+    const index = stars.indexOf(clickedStar);
+    stars.forEach((star, i) => {
+      if (i <= index) {
+        star.classList.add("checked");
+      } else {
+        star.classList.remove("checked");
+      }
+    });
+  }
+
+  function addHoverClass(event) {
+    const hoveredStar = event.target;
+    const stars = Array.from(hoveredStar.parentNode.children);
+    const index = stars.indexOf(hoveredStar);
+    stars.forEach((star, i) => {
+      if (i <= index) {
+        star.classList.add("hover");
+      } else {
+        star.classList.remove("hover");
+      }
+    });
+  }
+
+  function removeHoverClass(event) {
+    const hoveredStar = event.target;
+    const stars = Array.from(hoveredStar.parentNode.children);
+    stars.forEach(star => star.classList.remove("hover"));
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const favoriteIcon = document.querySelector(".nav-link .fas.fa-heart");
+
+  if (favoriteIcon) {
+    favoriteIcon.addEventListener("click", function() {
+      favoriteIcon.classList.add("fa-beat");
+
+      // Retire l'effet après 1 seconde
+      setTimeout(() => {
+        favoriteIcon.classList.remove("fa-beat");
+      }, 1000);
+    });
+  }
+});
