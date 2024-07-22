9097e77b04ac5f81562a5ba6b33d411b3e84956d
jonathan delvig <delvigjon@hotmail.fr>
2024-07-22 17:09:50 +0200

    prefcontrollers

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,6 +10,6 @@ class FavoritesController < ApplicationController
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
-    
+    redirect_to favorites_path, notice: 'Removed from favorites'
   end
 end
