11e7bea6d47b1656ab73ec5ad021735c84cc90dc
jonathan delvig <delvigjon@hotmail.fr>
2024-07-23 19:52:20 +0200

    test3

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,11 +4,21 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.coffee = @coffee
-    @review.save!
-    redirect_to coffee_path(@coffee)
+    @review.user = current_user # Assigner l'utilisateur actuel à la revue
+    # Inverser la valeur de la note
+    @review.rating = 6 - @review.rating if @review.rating.present?
+    
+    if @review.save
+      redirect_to coffee_path(@coffee), notice: 'Commentaire ajouté avec succès.'
+    else
+      redirect_to coffee_path(@coffee), alert: 'Erreur lors de l\'ajout du commentaire.'
+    end
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to coffee_path(@review.coffee), notice: 'Commentaire supprimé avec succès.'
   end
 
   private
